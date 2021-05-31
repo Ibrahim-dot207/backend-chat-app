e4faca5266398198cf7b1c81d3ca788e20d65904
Ibrahim-dot207 <marcourt207@gmail.com>
2021-05-31 13:00:51 +0000

    changes made in server folder

--- index.js
+++ index.js
@@ -1,15 +1,60 @@
-const app = require('express')()
-const http = require('http').createServer(app)
-const io = require ('socket.io')(http)
-
-io.on('connection',(socket)=>{
-    socket.on('message',({name, message})=>{
-        io.emit('message',{name, message})
-    })
-})
+const express = require('express');
+const cors = require('cors');
+const jwt = require('express-jwt');
+const jwks = require('jwks-rsa');
+const axios = require('axios');
 
 
-http.listen(4000, function(){
-    console.log('listening on port 4000')
+
+const app = express();
+app.use(cors());
+
+const verifyJwt =jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: 'https://marcourt207.eu.auth0.com/.well-known/jwks.json',
+    }),
+    audience: 'chatroom identifier',
+    issuer:'https://marcourt207.eu.auth0.com/',
+    algorithms:['RS256'],
+}).unless({path: ['/']});
+
+app.use(verifyJwt);
+
+app.get('/',(req, res)=> {
+    res.send('Hello from index route');
 });
 
+app.get('/protected', async (req, res)=>{
+    try{
+    const accessToken =req.headers.authorization.split('')[1]
+    const response = await axios.get('https://marcourt207.eu.auth0.com/userinfo', {
+        headers:{
+            authorization: `Bearer ${accessToken}`
+        }
+    })
+    const userinfo =response.data
+    console.log(userinfo)
+    res.send(userinfo);
+    } catch (error) {
+        res.send(error.message)
+    }
+});
+
+
+
+app.use((req, res, next)=>{
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+})
+
+app.use((error, req, res, next)=>{
+    const status = error.status || 500;
+    const message = error.message || 'internal server error';
+    res.status(status).send(message);
+})
+
+app.listen(4000, ()=> console.log('Server running on port 4000'));
